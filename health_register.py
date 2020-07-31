b89b2c4672481535d71478cafd987b5fa3609657
kainzhang <luvwind@qq.com>
2020-08-01 00:43:38 +0800

    调整挂起时间

--- health_register.py
+++ health_register.py
@@ -2,9 +2,9 @@ import datetime
 import json
 import os
 import smtplib
+import time
 from email.header import Header
 from email.mime.text import MIMEText
-import time
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -51,7 +51,7 @@ def login(driver, username, password):
 
         driver.find_element_by_id('tx_username').send_keys(username)
         driver.find_element_by_id('tx_password').send_keys(password)
-        time.sleep(1)
+        time.sleep(0.5)
         driver.find_element_by_class_name('login_btn').click()
 
         # 验证用户登录成功
@@ -65,7 +65,7 @@ def login(driver, username, password):
 
 
 def register(driver, user):
-    # 用户登录
+    # 用户登录并验证
     if login(driver, user['username'], user['password']) is False:
         return False
 
@@ -77,7 +77,7 @@ def register(driver, user):
         locator = (By.XPATH, './/div[@class="content"]/div[1]/a')
         WebDriverWait(driver, 30).until(EC.presence_of_element_located(locator))
 
-        # 进入并验证健康日报填写界面
+        # 验证进入健康日报填写界面
         driver.find_element_by_xpath('.//div[@class="content"]/div[1]/a').send_keys(Keys.ENTER)
         locator = (By.XPATH, 'html/body/div/div/div[3]/div[3]/div[10]/div[2]/div/div/div/input')
         WebDriverWait(driver, 30).until(EC.presence_of_element_located(locator))
@@ -85,165 +85,156 @@ def register(driver, user):
 
         # 修改地址获取方式为 h5
         driver.execute_script('now_from="h5"')
-        time.sleep(1)
 
         # 9 在校居住宿舍楼
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[3]/div[10]/div[2]/div/div/div/input'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
+
+        # 等待选项框弹出
+        locator = (By.XPATH, './/li[@role="button" and text()="东风校区"]')
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located(locator))
 
         # 选择宿舍楼
         if user['dormitory_loc'] == 'KX':
             driver.find_element_by_xpath(
                 './/li[@role="button" and text()="科学校区"]'
             ).click()
-            time.sleep(1)
+            time.sleep(0.5)
 
             driver.find_element_by_xpath(
                 './/li[@role="button" and text()="' + user['dormitory_bd'] + '号楼"]'
             ).click()
-            time.sleep(1)
+            time.sleep(0.5)
 
         elif user['dormitory_loc'] == 'ZD':
             driver.find_element_by_xpath(
                 './/li[@role="button" and text()="校外走读"]'
             ).click()
-            time.sleep(1)
+            time.sleep(0.5)
 
         # 确定宿舍楼信息
         driver.find_element_by_xpath(
             './/div[@class="van-picker"]/div/button[2]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 9-1 在校居住宿舍号
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[3]/div[11]/div/div[2]/div/div/div/input'
         ).send_keys(user['dormitory_num'])
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 10 联系电话 11位手机号
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[3]/div[12]/div[2]/div/div/div/input'
         ).send_keys(user['mobile_phone'])
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 10-1 家庭、家长 联系电话
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[3]/div[13]/div[2]/div/div/div/input'
         ).send_keys(user['home_phone'])
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 13 假期是否到过湖北以下地区（默认：无
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[3]/div[20]/div/div[2]/div/div/div/div/div/div[1]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 14 假期是否到过河南以下地区
-        flag = 0
         if user['xinyang'] == 1:
             driver.find_element_by_xpath(
                 'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[2]'
             ).click()
-            time.sleep(1)
-            flag = 1
+            time.sleep(0.5)
 
         if user['nanyang'] == 1:
             driver.find_element_by_xpath(
                 'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[3]'
             ).click()
-            time.sleep(1)
-            flag = 1
+            time.sleep(0.5)
 
         if user['zhumadian'] == 1:
             driver.find_element_by_xpath(
                 'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[4]'
             ).click()
-            time.sleep(1)
-            flag = 1
+            time.sleep(0.5)
 
         if user['shangqiu'] == 1:
             driver.find_element_by_xpath(
                 'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[5]'
             ).click()
-            time.sleep(1)
-            flag = 1
+            time.sleep(0.5)
 
         if user['zhoukou'] == 1:
             driver.find_element_by_xpath(
                 'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[6]'
             ).click()
-            time.sleep(1)
-            flag = 1
+            time.sleep(0.5)
 
-        if flag == 0:
-            driver.find_element_by_xpath(
-                'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[1]'
-            ).click()
-            time.sleep(1)
+        driver.find_element_by_xpath(
+            'html/body/div/div/div[3]/div[3]/div[22]/div/div[2]/div/div/div/div/div/div[1]'
+        ).click()
+        time.sleep(0.5)
 
         # 15 目前您的位置和居住地点（由浏览器自动获取
-        try:
-            locator = (By.XPATH, 'html/body/div/div/div[3]/div[5]/div/div[2]/div/div/div/textarea')
-            driver.find_element_by_xpath(
-                'html/body/div/div/div[3]/div[5]/div/div[2]//div/div/div/textarea'
-            ).click()
-            WebDriverWait(driver, 30).until(EC.text_to_be_present_in_element_value(locator, ''))
-        except TimeoutException:
-            print('地址获取超时')
-            return False
-
-        time.sleep(1)
+        locator = (By.XPATH, 'html/body/div/div/div[3]/div[5]/div/div[2]/div/div/div/textarea')
+        driver.find_element_by_xpath(
+            'html/body/div/div/div[3]/div[5]/div/div[2]//div/div/div/textarea'
+        ).click()
+        WebDriverWait(driver, 30).until(EC.text_to_be_present_in_element_value(locator, ''))
+        time.sleep(0.5)
 
         # 15-1 你获取的位置与本人目前所在地是否相符合（默认：是
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[5]/div[2]/div[2]/div/div[2]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 15-3 您所在的小区是否有确诊病例（默认：无
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[6]/div/div[2]/div/div[1]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 17 您今日有无以下症状（默认：无
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[10]/div[2]/div/div/div/div/div/div[1]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 18 今日同住人员身体状况（默认：无症状
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[12]/div[2]/div/div/div[1]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 18-2 您是否与疫区人员有过接触（默认：否
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[14]/div/div[2]/div/div[1]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 20-5 您是否曾经被诊断为确诊病例（默认：否
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[21]/div/div[2]/div/div[1]'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 21 其他需要说明的情况
         driver.find_element_by_xpath(
             'html/body/div/div/div[3]/div[26]/div[2]/div/div/div/textarea'
         ).send_keys(user['other_info'])
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 点击提交
         driver.find_element_by_xpath(
             './/div[@class="submit-div"]/button'
         ).click()
-        time.sleep(1)
+        time.sleep(0.5)
 
         # 确认提交
         driver.find_element_by_xpath(
